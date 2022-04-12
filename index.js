549eaa48d46b22eaef42e5cbfd59e37fdaf3e07e
Justin Lam <justin.kh.lam@gmail.com>
2022-04-12 12:49:04 -0400

    add in randomImageSource + comments

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const randomNumber1 = Math.floor(Math.random() * (6)) + 1;
+// picks from 1-6
 // console.log(randomNumber1);
 
-const randomDiceImage = `dice` + `${randomNumber1}` + `.png`;
+const randomDiceImage = `dice` + randomNumber1 + `.png`;
+// dice1.png - dice6.png
 // console.log(randomDiceImage)
+
+const randomImageSource = `images/` + randomDiceImage;
+// images/dice1.png - 6
 
