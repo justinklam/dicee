bb56e577dbe1d039ee395833aab300b5a6b4bd7c
Justin Lam <justin.kh.lam@gmail.com>
2022-04-12 12:46:16 -0400

    set a variable to select a random dice image

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 const randomNumber1 = Math.floor(Math.random() * (6)) + 1;
-
 // console.log(randomNumber1);
+
+const randomDiceImage = `dice` + `${randomNumber1}` + `.png`;
+// console.log(randomDiceImage)
+
