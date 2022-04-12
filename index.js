81ab17e3403a22fe66498046e2817632d21772c1
Justin Lam <justin.kh.lam@gmail.com>
2022-04-12 12:43:17 -0400

    convert function into variable

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const randomNumber1 = function() {
-  return Math.floor(Math.random() * (6) + 1);
-};
+const randomNumber1 = Math.floor(Math.random() * (6)) + 1;
 
-console.log(randomNumber1());
+// console.log(randomNumber1);
